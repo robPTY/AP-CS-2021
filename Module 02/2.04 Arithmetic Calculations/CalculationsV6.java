924617ac4c6ce24dd355d46a148fa710d2500d92
retrQJS <108301046+retrQJS@users.noreply.github.com>
2022-07-06 08:33:40 -0500

    Create CalculationsV6.java

---
+++ Module 02/2.04 Arithmetic Calculations/CalculationsV6.java
@@ -0,0 +1,115 @@
+
+/**
+ * CalculationsV6 is the assignment that has taken place from 2.02 onward and it deals with integers and doubles. 
+ * 
+ * 
+ * @author (Roberto Aguero) 
+ * @version (August 26,2020)
+ */
+public class CalculationsV6
+{
+       public static void main(String[ ] args)
+    {
+        
+        // Declare integer variables
+        int iNum1 = 25;
+        int iNum2 = 9;
+        
+        // Declare double variables
+        double dNum1 =43.21;
+        double dNum2 =3.14;
+        double dNum3 =10.0;
+        
+        // Addition
+        System.out.println("Addition");
+        System.out.println(iNum1 + " + " + iNum2 + " = " + ( iNum1 + iNum2));
+        System.out.println(dNum1 + " + " + dNum2 + " + " + dNum3 + " = "+( dNum1 + dNum2 + dNum3 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum3 = 11;
+        int iNum4 = 9;
+       int iNum5 =25;
+       
+       
+        // Subtraction
+        System.out.println("Subtraction");
+        System.out.println(iNum3 + " - " + iNum4 + " - " + iNum5 + " = " +( iNum3 - iNum4 - iNum5 ));
+        System.out.println(dNum2 + " - " + dNum3 + " = "+( dNum2 - dNum3 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum6 = 25;
+        int iNum7 = 9;
+
+        // Multiplication
+        System.out.println("Multiplication");
+        System.out.println(iNum6 + " * " + iNum7 + " = " +( iNum6 * iNum7 )); 
+        System.out.println(dNum2 + " * " + dNum3 + " * " + dNum3 + " = "+(dNum2 * dNum3 *dNum3 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum8 = 9;
+        int iNum9 = 25;
+        // Division
+        System.out.println("Division");
+        System.out.println(iNum8 + " / " + iNum9 + " =  " +( iNum8 / iNum9 ));
+        System.out.println(dNum1 + " / " + dNum3 + " = "+( dNum1 / dNum3 ));
+        //System.out.println();
+         
+        //Declare integer variables
+        int iNum10 = 11;
+        int iNum11 = 9;
+        // Modulus operator
+        System.out.println("Modulus");
+        System.out.println(iNum10 + " % " + iNum11 +" = "+( iNum10 % iNum11 ));
+        System.out.println(dNum3 + " % " + dNum2 + " = "+( dNum3 % dNum2 ));
+        //System.out.println();
+        
+       
+        // 2.03 Additional int Equations
+        
+        //Declare integer variables
+        int iNum12 = 12;
+        int iNum13 = 2;
+        int iNum14 = 3;
+        System.out.println("Additional int #1");
+        System.out.println(iNum12 + " + " + iNum13 + " - " + iNum14 + " = "+(iNum12 + iNum13 - iNum14 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum15 = 5;
+        int iNum16 = 4;
+       int iNum17 = 2;
+       System.out.println("Additional int #2");
+       System.out.println(iNum15 + " * " + iNum16 + " / " +iNum17 + " = "+(iNum15 * iNum16 / iNum17));
+       
+       //Declare integer variables
+       int iNum18 = 50;
+       int iNum19 = 25;
+       int iNum20 = 5;
+       System.out.println("Additional int #3");
+       System.out.println(iNum18 + " / " + iNum19 + " - " + iNum20 + " = "+(iNum18 / iNum19 - iNum20));
+        //2.04 Additional double Equations
+         
+        //Declare double variables
+        double dNum4 = 5;
+        double dNum5 = 55;
+        System.out.println("Additional double #1");
+        System.out.println(dNum4 + " * " + dNum5 + " = "+(dNum4 * dNum5));
+        
+        //Declare double variables
+        double dNum6 = 77;
+        double dNum7 = 11;
+        System.out.println("Additional double #2");
+        System.out.println(dNum6 + " / " + dNum7 + " = "+(dNum6 / dNum7));
+        
+        //Declare double variables
+        double dNum8 =12;
+       double dNum9 =4;
+       double dNum10 =200;
+       System.out.println("Additional double #3");
+       System.out.println(dNum8 + " / " + dNum9 + " * " + dNum10 + " = "+(dNum8 / dNum9 * dNum10));
+       
+    } // end of main method
+}
