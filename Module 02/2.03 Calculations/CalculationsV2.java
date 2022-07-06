ed22b137dae7a02830234d571506cb3fe4918d09
retrQJS <108301046+retrQJS@users.noreply.github.com>
2022-07-06 08:31:39 -0500

    Create CalculationsV2.java

---
+++ Module 02/2.03 Calculations/CalculationsV2.java
@@ -0,0 +1,66 @@
+
+/**
+ * Write a description of class CalculationsV4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalculationsV4
+{
+    public static void main(String[ ] args)
+    {
+        
+        // Declare integer variables
+        int iNum1 = 25;
+        int iNum2 = 9;
+        // Addition
+        System.out.println("Addition");
+        System.out.println(iNum1 + " + " + iNum2 + " = " + ( iNum1 + iNum2));
+        System.out.println("43.21 + 3.14 + 10.0 = "+( 43.21 + 3.14 + 10.0 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum3 = 11;
+        int iNum4 = 9;
+       int iNum5 =25;
+        // Subtraction
+        System.out.println("Subtraction");
+        System.out.println(iNum3 + " - " + iNum4 + " - " + iNum5 + " = " +( iNum3 - iNum4 - iNum5 ));
+        System.out.println("3.14 - 10.0 = "+( 3.14 - 10.0 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum6 = 25;
+        int iNum7 = 9;
+        // Multiplication
+        System.out.println("Multiplication");
+        System.out.println(iNum6 + " * " + iNum7 + " = " +( iNum6 * iNum7 )); 
+        System.out.println("3.14 * 10.0 * 10.0 ="+(3.14 * 10.0 * 10.0 ));
+        //System.out.println();
+        
+        //Declare integer variables
+        int iNum8 = 9;
+        int iNum9 = 25;
+        // Division
+        System.out.println("Division");
+        System.out.println(iNum8 + " / " + iNum9 + " =  " +( iNum8 / iNum9 ));
+        System.out.println("43.21 / 10.0 ="+( 43.21 / 10.0 ));
+        //System.out.println();
+         
+        //Declare integer variables
+        int iNum10 = 11;
+        int iNum11 = 9;
+        // Modulus operator
+        System.out.println("Modulus");
+        System.out.println(iNum10 + " % " + iNum11 +" = "+( iNum10 % iNum11 ));
+        System.out.println("10.0 % 3.14 ="+( 10.0 % 3.14 ));
+        //System.out.println();
+        
+        // 2.03 Additional int Equations
+        
+        
+        //2.04 Additional double Equations
+        
+               
+    } // end of main method
+}
