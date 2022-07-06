0dcc23507f358d3df8bf2599e8da7d4d616d62b2
retrQJS <108301046+retrQJS@users.noreply.github.com>
2022-07-06 08:36:46 -0500

    Create GradesV2.java

---
+++ Module 02/2.07 Grades/GradesV2.java
@@ -0,0 +1,136 @@
+
+/**
+ * The purpose of this project is to know the total amount of points and the average grade after each test grade.
+ * 
+ * @author (Roberto Aguero) 
+ * @version (August 31,2020)
+ */
+public class GradesV2
+{
+    public static void main(String[ ] args)
+    {
+         //local variables
+        int numTests = 0;      //counts number of tests
+        int testGrade = 0;     //individual test grade
+        int totalPoints = 0;   //total points for all tests
+        double average = 0.0;  //average grade
+        
+  double xd = (3 + 4 / (double)3) + 5;
+  System.out.print(xd);
+   
+        int grade1 = 88;
+int grade2 = 92;
+int grade3 = 83;
+
+String str = "Florida Keys";
+System.out.print(str.indexOf("eys"));
+
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = 95.0/1;
+        System.out.println(" Average Score: " + average);
+        
+         testGrade = 73;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0) /2;
+        System.out.println(" Average Score: " + average);
+        
+         testGrade = 91;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0) /3;
+        System.out.println(" Average Score: " + average);
+        
+        testGrade = 82;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0 +82.0) /4;
+        System.out.println(" Average Score: " + average);
+        
+        testGrade = 97;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0 +82.0 + 97.0) /5;
+        System.out.println(" Average Score: " + average);
+        
+          testGrade = 67;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0 +82.0 + 97.0 + 67.0) /6;
+        System.out.println(" Average Score: " + average);
+        
+        
+          testGrade = 88;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0 +82.0 + 97.0 + 67.0 + 88.0) /7;
+        System.out.println(" Average Score: " + average);
+        
+         testGrade = 84;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0 +82.0 + 97.0 + 67.0 + 88.0 + 84.0) /8;
+        System.out.println(" Average Score: " + average);
+        
+         testGrade = 99;
+        numTests++;
+        totalPoints += testGrade;
+        System.out.print(" Test #" + numTests);
+        System.out.print(" ");
+        System.out.print(" Test Grade: " + testGrade);
+        System.out.print(" ");
+        System.out.print(" Total Points: " + totalPoints);
+        System.out.print(" ");
+        average = (95.0 + 73.0 + 91.0 +82.0 + 97.0 + 67.0 + 88.0 + 84.0 + 99.0) /9;
+        System.out.println(" Average Score: " + average);
+    }//end of main method
+}
