f6cb427cd42e350eccaadc9b3c1d32d6928672c8
retrQJS <108301046+retrQJS@users.noreply.github.com>
2022-07-06 08:40:24 -0500

    Create Art.java

---
+++ Module 03/Module 03 Assignments/3.04 ASCII Art/Art.java
@@ -0,0 +1,46 @@
+/**
+ * This class contains the artwork of a sailboat that I made using string objects.
+ * 
+ * @author (Roberto Aguero) 
+ * @version (September 4,2020)
+ */
+public class ArtWork
+{
+   public static void main(String[ ] args)
+   {
+       //local variables
+       String row01 = "               _          \n";
+       String row02 = "              / \\        \n";
+       String row03 = "             / | \\       \n";
+       String row04 = "            /  |  \\      \n";
+       String row05 = "           /   |   \\     \n";
+       String row06 = "          /    |    \\    \n"; 
+       String row07 = "         /     |     \\   \n";
+       String row08 = "        /      |      \\  \n";
+       String row09 = "        ----------------  \n";
+       String row10 = "               |          \n";
+       String row11 = "        ----------------- \n";
+       String row12 = "        \\              / \n";
+       String row13 = "         \\            /  \n";
+       String row14 = "          \\__________/   \n";
+       String row15 = " By: Roberto Aguero";
+       
+       //print ASCII art
+       System.out.print(row01);
+       System.out.print(row02);
+       System.out.print(row03);
+       System.out.print(row04);
+       System.out.print(row05);
+       System.out.print(row06);
+       System.out.print(row07);
+       System.out.print(row08);
+       System.out.print(row09);
+       System.out.print(row10);
+       System.out.print(row11);
+       System.out.print(row12);
+       System.out.print(row13);
+       System.out.print(row14);
+       
+       System.out.print(row15);
+     }
+}
